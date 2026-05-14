8a4feb86ff3bee63f09551ee5a50bd8ae4cc5953
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2026-05-14 16:32:42 +0800

    Update timm import compatibility

--- models/vit.py
+++ models/vit.py
@@ -30,12 +30,21 @@ import torch.nn.functional as F
 import torch.utils.checkpoint
 from timm.data import (IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD,
                        IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD)
-from timm.models.helpers import (adapt_input_conv, build_model_with_cfg,
-                                 checkpoint_seq, named_apply,
-                                 resolve_pretrained_cfg)
-from timm.models.layers import (DropPath, Mlp, PatchEmbed, lecun_normal_,
-                                trunc_normal_)
-from timm.models.registry import register_model
+try:
+    from timm.models import (adapt_input_conv, build_model_with_cfg,
+                             checkpoint_seq, named_apply, register_model,
+                             resolve_pretrained_cfg)
+except ImportError:
+    from timm.models.helpers import (adapt_input_conv, build_model_with_cfg,
+                                     checkpoint_seq, named_apply,
+                                     resolve_pretrained_cfg)
+    from timm.models.registry import register_model
+
+try:
+    from timm.layers import DropPath, Mlp, PatchEmbed, lecun_normal_, trunc_normal_
+except ImportError:
+    from timm.models.layers import (DropPath, Mlp, PatchEmbed, lecun_normal_,
+                                    trunc_normal_)
 
 _logger = logging.getLogger(__name__)
 
